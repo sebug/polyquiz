be94ba1d32825fc9c6e1f517c3d8df917c06a591
sebug <sebastian.gfeller@gmail.com>
2015-09-29 19:00:42 +0200

    Initial setup

---
+++ src/main/java/polyquiz/PolyquizApplication.java
@@ -0,0 +1,12 @@
+package polyquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyquizApplication.class, args);
+    }
+}
