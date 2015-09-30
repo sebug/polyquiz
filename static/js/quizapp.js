978c293bd4da5d3f7fed387d04f6cfc50ca7d1a8
sebug <sebastian.gfeller@gmail.com>
2015-09-30 08:49:18 +0200

    With list

--- static/js/quizapp.js
+++ static/js/quizapp.js
@@ -1,9 +1,39 @@
 (function () {
     'use strict';
-    var viewModel = {
+    var getRequest,
+	fetchQuizAnswers,
+	fetchQuestions,
+	viewModel;
+
+    getRequest = function (url) {
+	return function (andThen) {
+	    var invocation = new XMLHttpRequest();
+	    invocation.open('GET', url, true);
+	    invocation.setRequestHeader('Content-Type', 'application/json');
+	    if (typeof andThen === 'function') {
+		invocation.onload = function () {
+		    andThen(JSON.parse(this.responseText));
+		};
+	    }
+	    invocation.send();
+	};
+    };
+
+    fetchQuizAnswers = getRequest('/quiz_answer/');
+    fetchQuestions = getRequest('/question/');
+    
+    viewModel = {
 	quizAnswers: ko.observableArray([]),
 	questions: ko.observableArray([])
     };
+
+    fetchQuizAnswers(function (answers) {
+	viewModel.quizAnswers(answers);
+    });
+
+    fetchQuestions(function (questions) {
+	viewModel.questions(questions);
+    });
 
     ko.applyBindings(viewModel);
 }());
