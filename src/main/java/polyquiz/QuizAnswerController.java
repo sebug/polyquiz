05c1f560d6dce61b3f4ecb5f573f37e9d3207612
sebug <sebastian.gfeller@gmail.com>
2015-10-03 13:10:44 +0200

    All before serialize

--- src/main/java/polyquiz/QuizAnswerController.java
+++ src/main/java/polyquiz/QuizAnswerController.java
@@ -12,6 +12,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 @RestController
 @RequestMapping(value="quiz_answer")
@@ -39,6 +41,30 @@ public class QuizAnswerController {
 	return this.quizAnswerStoreBean.getQuizAnswersByFullName(fullname);
     }
 
+    @RequestMapping(value="/duplicates/")
+    public List<String> getDuplicates() {
+	List<String> duplicateNames = new ArrayList<String>();
+	HashSet<String> names = new HashSet<String>();
+	List<QuizAnswer> allAnswers = this.quizAnswerStoreBean.getAllQuizAnswers();
+	for (QuizAnswer answer : allAnswers) {
+	    if (names.contains(answer.getFullName())) {
+		duplicateNames.add(answer.getFullName());
+	    } else {
+		names.add(answer.getFullName());
+	    }
+	}
+
+	return duplicateNames;
+    }
+
+    @RequestMapping(value="/favelet/{id}")
+    public String getFavelet(@PathVariable String id) {
+	log.info("Calling getFavelet " + id);
+	QuizAnswer qa = this.quizAnswerStoreBean.getQuizAnswerById(id);
+	
+	return faveletPlaceholder;
+    }
+
     @RequestMapping(value="/create")
     public QuizAnswer create(@RequestBody QuizAnswer content) {
 	log.info(content);
@@ -50,4 +76,71 @@ public class QuizAnswerController {
 	this.quizAnswerStoreBean.storeQuizAnswer(content);
 	return content;
     }
+
+    private String faveletPlaceholder = "(function () {" +
+	"    'use strict';" +
+	"    var mainFrame = document.getElementsByTagName('frame')[1]," +
+	"	tr," +
+	"	trs," +
+	"	i," +
+	"	j," +
+	"	q," +
+	"	a," +
+	"	selectedQuestion," +
+	"	questionText," +
+	"	answerText," +
+	"	inputs," +
+	"	nameInput," +
+	"	confirmName," +
+	"	auswertenButton," +
+	"	entry = %%ENTRY%%;" +
+	"" +
+	"    inputs = mainFrame.contentDocument.getElementsByTagName('input');" +
+	"" +
+	"    if (inputs.length > 1 && inputs[0].type === 'text') {" +
+	"	nameInput = inputs[0];" +
+	"	confirmName = inputs[1];" +
+	"    }" +
+	"" +
+	"    if (mainFrame.contentDocument.getElementsByTagName('b').length > 0) {" +
+	"	questionText = mainFrame.contentDocument.getElementsByTagName('b')[0].innerText;" +
+	"    }" +
+	"" +
+	"    // Find answer" +
+	"    selectedQuestion = undefined;" +
+	"    for (i = 0; i < entry.questionAnswers.length && !selectedQuestion; i += 1) {" +
+	"	q = entry.questionAnswers[i];" +
+	"	if (q.question === questionText) {" +
+	"	    selectedQuestion = q;" +
+	"	}" +
+	"    }" +
+	"" +
+	"    if (selectedQuestion) {" +
+	"	console.log(\"Found question\");" +
+	"	trs = mainFrame.contentDocument.getElementsByTagName('tr');" +
+	"	for (i = 0; i < trs.length; i += 1) {" +
+	"	    tr = trs[i];" +
+	"	    answerText = tr.getElementsByTagName('td')[1].innerText.replace(/\\./g,'');" +
+	"	    for (j = 0; j < selectedQuestion.answer.length; j += 1) {" +
+	"		a = selectedQuestion.answer[j];" +
+	"		a = a.replace(/\\./g, '');" +
+	"		if (a === answerText) {" +
+	"		    tr.getElementsByTagName('td')[0].getElementsByTagName('input')[0].click();" +
+	"		}" +
+	"	    }" +
+	"	}" +
+	"" +
+	"	inputs = mainFrame.contentDocument.getElementsByTagName('input');" +
+	"	for (i = 0; i < inputs.length; i += 1) {" +
+	"	    if (inputs[i].value === \" Evaluer \") {" +
+	"		inputs[i].click();" +
+	"	    }" +
+	"	}" +
+	"    }" +
+	"" +
+	"    if (nameInput && confirmName) {" +
+	"	nameInput.value = entry.fullName;" +
+	"	confirmName.click();" +
+	"    }" +
+	"}());";
 }
